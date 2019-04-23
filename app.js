a10e5708ed6756c1fc0b11fc9f308ba48d4ffc1a
victoriajgreen <victoriajgreen@gmail.com>
2019-04-23 00:00:38 -0600

    pointing at heroku now

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket('ws:https://websockethangman.herokuapp.com/');
 
 // socket.onmessage = function (event) {
 //   var received = event.data;
